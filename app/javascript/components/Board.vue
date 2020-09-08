43a6ae6ad26c892ac1bc57548fef6a6e58b216a7
tomostea <tomostea@gmail.com>
2020-09-08 11:58:52 +0900

    コンポーネント作成

---
+++ app/javascript/components/Board.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="bg-info">
+    <h4>
+    <div class="container p-1">
+      <div class="row">
+        <div
+          class="col-12 col-md-4 col-lg-3"
+          v-for="(items, list_title) in lists"
+          :key="list_title"
+        >
+          <List :items="items" :list_title="list_title" />
+        </div>
+        <div class="col-12 col-md-4 col-lg-3">
+          <button type="button" class="btn btn-outline-light d-flex align-self-center"><span class="material-icons">add</span> リストを追加する</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import List from "./List";
+
+export default {
+  name: "Board",
+  data() {
+    return {
+      lists: {
+        TODO: ["今日の献立を考える", "ご飯を食べる"],
+        Doing: ["天気を予想する"],
+        Done: ["家計簿をつける", "予算を立てる"],
+      },
+    };
+  },
+  components: {
+    List,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@media only screen and (max-width:768px){
+    .box{margin-bottom:10px;}
+}
+</style>
---
+++ app/javascript/components/Item.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card">
+    <div class="d-flex">
+      <div class="p-2 align-self-center w-100">{{item}}</div>
+      <div class="p-2 flex-shrink-1">
+        <button type="button" class="btn btn-outline-info">
+          <span class="material-icons">info</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Item",
+  props: {
+    item: {
+      type: String,
+      default: "item",
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
