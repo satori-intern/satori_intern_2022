5725ae2634e140d5103372a431acdad8e9ba030a
tomostea <tomostea@gmail.com>
2020-09-08 14:15:04 +0900

    modal関連の処理追加

--- app/javascript/components/Item.vue
+++ app/javascript/components/Item.vue
@@ -3,17 +3,28 @@
     <div class="d-flex">
       <div class="p-2 align-self-center w-100">{{item.name}}</div>
       <div class="p-2 flex-shrink-1">
-        <button type="button" class="btn btn-outline-info align-self-center">
+        <button type="button" class="btn btn-outline-info align-self-center" @click="modalBtn">
           <span class="material-icons">info</span>
         </button>
       </div>
     </div>
+    <p v-if="showModal" :item="item">モーダル表示</p> <!-- dummy -->
   </div>
 </template>
 
 <script>
 export default {
   name: "Item",
+  data () {
+    return {
+      showModal: false
+    }
+  },
+  methods: {
+    modalBtn () {
+      this.showModal = this.showModal ? false : true
+    }
+  },
   props: {
     item: {
       type: Object,
