d847a24293d42ee949bc279f69df9bbb13061d30
noimica <33836285+noimica@users.noreply.github.com>
2020-09-09 15:28:22 +0900

    Revert "Back 13 add rearranging operation"

--- app/models/board.rb
+++
@@ -1,3 +0,0 @@
-class Board < ApplicationRecord
-    has_many :lists, dependent: :destroy
-end
--- app/models/list.rb
+++
@@ -1,6 +0,0 @@
-class List < ApplicationRecord
-    belongs_to :board
-    has_many :items, dependent: :destroy
-
-    validates :board_id, numericality: true
-end
