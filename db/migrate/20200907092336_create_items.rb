c6a4642513be7ae68ecf9f6144e34c93738253fd
noimica <noimica.l@gmail.com>
2020-09-09 05:16:18 +0000

    fixed migrate files

--- db/migrate/20200907092336_create_items.rb
+++ db/migrate/20200907092336_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.string :name
-      t.integer :board_id, :limit => 8
+      t.integer :list_id, :limit => 8
       t.integer :index, :limit => 8
       t.text :detail
 
--- db/migrate/20200908070108_rename_board_id_column_to_items.rb
+++
@@ -1,5 +0,0 @@
-class RenameBoardIdColumnToItems < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :items, :board_id, :list_id
-  end
-end
