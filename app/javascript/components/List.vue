07f827babcdb491a1047e4692d9889eabc85df52
tomostea <tomostea@gmail.com>
2020-09-10 11:14:51 +0900

    10から16に移動 (List)

--- app/javascript/components/List.vue
+++ app/javascript/components/List.vue
@@ -1,108 +1,120 @@
 <template>
-<div class="card">
+  <div class="card">
     <div class="card-body bg-light">
-        <h4 class="card-title">{{title}}</h4>
-        <draggable v-model="items" :key="listId" group="board" @start="moveId">
-            <div v-for="item in items" :key="item.id">
-                <Item :item-item="item" />
-            </div>
-        </draggable>
-        <button type="button" class="btn btn-outline-info d-flex align-self-center">
-            <span class="material-icons">add</span> アイテムを追加
-        </button>
+      <h4 class="card-title">{{title}}</h4>
+      <draggable v-model="items" :key="listId" group="board" @start="moveId">
+        <div v-for="item in items" :key="item.id">
+          <Item :item-item="item" />
+        </div>
+      </draggable>
+      <AddBtn @catchNewName="addItem" :add-type="addType" />
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 import Item from "./Item";
+import AddBtn from "./AddBtn";
+import axios from "axios";
 import draggable from "vuedraggable";
+
 export default {
-    name: "List",
-    components: {
-        Item,
-        draggable,
+  name: "List",
+  components: {
+    Item,
+    draggable,
+    AddBtn,
+  },
+  props: {
+    listTitle: {
+      type: String,
+      default: "",
     },
-    props: {
-        listTitle: {
-            type: String,
-            default: "",
-        },
-        listItems: {
-            type: Array,
-            default: () => {
-                return [];
-            },
-        },
-        id: {
-            type: Number,
-            default: 1,
-        },
+    listItems: {
+      type: Array,
+      default: () => {
+        return [];
+      },
     },
-    data() {
-        return {
-            title: this.listTitle,
-            items: this.listItems,
-            listId: this.id,
-            moveOldIndex: 0,
-        };
+    id: {
+      type: Number,
+      default: 1,
     },
-    created() {
-        this.$watch(
-            () => [this.items],
-            // valueやoldValueの型は上で返した配列になる
-            (val, oldval) => {
-                this.moveItem(val, oldval);
-            }
-        );
+    listTitle: {
+      type: String,
     },
-    methods: {
-        moveItem: function (val, oldVal) {
-            const oldLists = oldVal[0];
-            const newList = val[0];
-            let beforeMoveId;
-            let afterMoveId;
-            //減ったほうのlists
-            if (newList.length < oldLists.length) return;
-            //増えた方のlists
-            if (newList.length > oldLists.length) {
-                //動かすアイテムのid
-                beforeMoveId = newList.filter((elem) => {
-                    return !oldLists.some((list) => {
-                        return elem.id === list.id;
-                    });
-                })[0].id;
-                //動かしたアイテムのidのインデックス
-                const movedIdIndex = newList.findIndex(({
-                    id
-                }) => id === beforeMoveId);
-                //動かしたアイテムのidのひとつ上
-                afterMoveId =
-                    movedIdIndex - 1 >= 0 ? newList[movedIdIndex - 1].id : null;
-                console.log(beforeMoveId, afterMoveId);
-            }
-            //同じリスト内の移動のとき
-            if (newList.length === oldLists.length) {
-                beforeMoveId = oldLists[this.moveOldIndex].id;
-                //動かしたアイテムのidのインデックス
-                const movedIdIndex = newList.findIndex(({
-                    id
-                }) => id === beforeMoveId);
-                //動かしたアイテムのidのひとつ上
-                afterMoveId =
-                    movedIdIndex - 1 >= 0 ? newList[movedIdIndex - 1].id : null;
-                console.log(beforeMoveId, afterMoveId);
-            }
-            return beforeMoveId, afterMoveId;
-        },
-        moveId: function (event) {
-            this.moveOldIndex = event.oldIndex;
-        },
+  },
+  data() {
+    return {
+      title: this.listTitle,
+      items: this.listItems,
+      listId: this.id,
+      moveOldIndex: 0,
+      newName: "",
+      addType: "アイテム",
+    };
+  },
+  created() {
+    this.$watch(
+      () => [this.items],
+      // valueやoldValueの型は上で返した配列になる
+      (val, oldval) => {
+        this.moveItem(val, oldval);
+      }
+    );
+  },
+  methods: {
+    addItem: function (newName) {
+      const addListId = this.listId;
+      const newItemId = axios
+        .post("/items/create", { name: newName, list_id: addListId })
+        .then((res) => res.data.id)
+        .then((newItemId) => {
+          const newItem = {
+            id: newItemId,
+            name: newName,
+            detail: "",
+          };
+          this.items.push(newItem);
+        });
     },
+    moveItem: function (val, oldVal) {
+      const oldLists = oldVal[0];
+      const newList = val[0];
+      let beforeMoveId;
+      let afterMoveId;
+      //減ったほうのlists
+      if (newList.length < oldLists.length) return;
+      //増えた方のlists
+      if (newList.length > oldLists.length) {
+        //動かすアイテムのid
+        beforeMoveId = newList.filter((elem) => {
+          return !oldLists.some((list) => {
+            return elem.id === list.id;
+          });
+        })[0].id;
+        //動かしたアイテムのidのインデックス
+        const movedIdIndex = newList.findIndex(({ id }) => id === beforeMoveId);
+        //動かしたアイテムのidのひとつ上
+        afterMoveId =
+          movedIdIndex - 1 >= 0 ? newList[movedIdIndex - 1].id : null;
+        console.log(beforeMoveId, afterMoveId);
+      }
+      //同じリスト内の移動のとき
+      if (newList.length === oldLists.length) {
+        beforeMoveId = oldLists[this.moveOldIndex].id;
+        //動かしたアイテムのidのインデックス
+        const movedIdIndex = newList.findIndex(({ id }) => id === beforeMoveId);
+        //動かしたアイテムのidのひとつ上
+        afterMoveId =
+          movedIdIndex - 1 >= 0 ? newList[movedIdIndex - 1].id : null;
+        console.log(beforeMoveId, afterMoveId);
+      }
+      return beforeMoveId, afterMoveId;
+    },
+    moveId: function (event) {
+      this.moveOldIndex = event.oldIndex;
+    },
+  },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-
-<style scoped>
-</style>
