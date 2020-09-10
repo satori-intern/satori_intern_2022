fedccdd6dca48af5bc0d0603569743b8c6aed34e
yunine16 <yuchin616@icloud.com>
2020-09-10 02:10:01 +0000

    create assignmentDB

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,6 @@
 class Board < ApplicationRecord
     has_many :lists, dependent: :destroy
+    has_many :users, through: :assignments
+    has_many :assignments
+    accepts_nested_attributes_for :assignments
 end
