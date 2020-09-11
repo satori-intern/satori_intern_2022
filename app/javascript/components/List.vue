ed71bf9d3e20159c66ffad871dbb3c1a1febb160
hayaki821 <khkur821@i.softbank.jp>
2020-09-11 01:43:15 +0000

    draggable worninng　修正

--- app/javascript/components/List.vue
+++ app/javascript/components/List.vue
@@ -1,21 +1,15 @@
 <template>
-  <div class="card">
+<div class="card">
     <div class="card-body bg-light">
-      <h4 class="card-title">{{title}}</h4>
-      <draggable
-        v-model="list.items"
-        :key="list.id"
-        group="board"
-        @start="moveId"
-        :options="{animation:500}"
-      >
-        <div v-for="item in list.items" :key="item.id">
-          <Item :item-copy="item" @ItemToListInfo=ItemToListInfo />
-        </div>
-      </draggable>
-      <AddBtn @catchNewName="addItem" :add-type="addType" />
+        <h4 class="card-title">{{title}}</h4>
+        <draggable v-model="list.items" :key="list.id" group="board" @start="moveId" :animation="500">
+            <div v-for="item in list.items" :key="item.id">
+                <Item :item-copy="item" @ItemToListInfo="ItemToListInfo" />
+            </div>
+        </draggable>
+        <AddBtn @catchNewName="addItem" :add-type="addType" />
     </div>
-  </div>
+</div>
 </template>
 
 <script>
@@ -23,109 +17,112 @@ import Item from "./Item";
 import AddBtn from "./AddBtn";
 import axios from "axios";
 import draggable from "vuedraggable";
-
 export default {
-  name: "List",
-  components: {
-    Item,
-    draggable,
-    AddBtn,
-  },
-  props: {
-    listCopy: {
-      type: Object,
-      default: () => {
-        return {};
-      },
-    }
-  },
-  data() {
-    return {
-      title: this.listCopy.name,
-      list: this.listCopy,
-      moveOldIndex: 0,
-      newName: "",
-      addType: "アイテム",
-    };
-  },
-  created() {
-    this.$watch(
-      () => [this.list.items],
-      // valueやoldValueの型は上で返した配列になる
-      (val, oldval) => {
-        this.moveItem(val, oldval);
-      },
-      {
-        deep: true,
-      }
-    );
-  },
-  methods: {
-    addItem: function (newName) {
-      const addListId = this.list.id;
-      const newItemId = axios
-        .post("/items/create", { name: newName, list_id: addListId })
-        .then((res) => res.data.id)
-        .then((newItemId) => {
-          const newItem = {
-            id: newItemId,
-            name: newName,
-            detail: "",
-          };
-          this.list.items.push(newItem);
-        });
+    name: "List",
+    components: {
+        Item,
+        draggable,
+        AddBtn,
     },
-    moveItem: function (val, oldVal) {
-      const oldLists = oldVal[0];
-      const newLists = val[0];
-
-      //減ったほうのlists
-      if (newLists.length < oldLists.length) return;
-      let beforeMoveId;
-      let afterMoveId;
-
-      //増えた方のlists
-      if (newLists.length > oldLists.length) {
-        //動かすアイテムのid
-        beforeMoveId = newLists.filter((elem) => {
-          return !oldLists.some((list) => {
-            return elem.id === list.id;
-          });
-        })[0].id;
-        //動かしたアイテムのidのインデックス
-        const movedIdIndex = newLists.findIndex(
-          ({ id }) => id === beforeMoveId
+    props: {
+        listCopy: {
+            type: Object,
+            default: () => {
+                return {};
+            },
+        },
+    },
+    data() {
+        return {
+            title: this.listCopy.name,
+            list: this.listCopy,
+            moveOldIndex: 0,
+            newName: "",
+            addType: "アイテム",
+        };
+    },
+    created() {
+        this.$watch(
+            () => [this.list.items],
+            // valueやoldValueの型は上で返した配列になる
+            (val, oldval) => {
+                this.moveItem(val, oldval);
+            }, {
+                deep: true,
+            }
         );
-        //動かしたアイテムのidのひとつ上
-        afterMoveId =
-          movedIdIndex - 1 >= 0 ? newLists[movedIdIndex - 1].id : null;
-      }
-      //同じリスト内の移動のとき
-      if (newLists.length === oldLists.length) {
-        beforeMoveId = oldLists[this.moveOldIndex].id;
-        //動かしたアイテムのidのインデックス
-        const movedIdIndex = newLists.findIndex(
-          ({ id }) => id === beforeMoveId
-        );
-        //動かしたアイテムのidのひとつ上
-        afterMoveId =
-          movedIdIndex - 1 >= 0 ? newLists[movedIdIndex - 1].id : null;
-      }
-      let changeListId = this.list.id;
-      axios
-        .post("/items/move", {
-          id: beforeMoveId,
-          to_id: afterMoveId,
-          list_id: changeListId,
-        })
-        .then((response) => {});
     },
-    moveId: function (event) {
-      this.moveOldIndex = event.oldIndex;
+    methods: {
+        addItem: function (newName) {
+            const addListId = this.list.id;
+            const newItemId = axios
+                .post("/items/create", {
+                    name: newName,
+                    list_id: addListId,
+                })
+                .then((res) => res.data.id)
+                .then((newItemId) => {
+                    const newItem = {
+                        id: newItemId,
+                        name: newName,
+                        detail: "",
+                    };
+                    this.list.items.push(newItem);
+                });
+        },
+        moveItem: function (val, oldVal) {
+            const oldLists = oldVal[0];
+            const newLists = val[0];
+            //減ったほうのlists
+            if (newLists.length < oldLists.length) return;
+            let beforeMoveId;
+            let afterMoveId;
+            //増えた方のlists
+            if (newLists.length > oldLists.length) {
+                //動かすアイテムのid
+                beforeMoveId = newLists.filter((elem) => {
+                    return !oldLists.some((list) => {
+                        return elem.id === list.id;
+                    });
+                })[0].id;
+                //動かしたアイテムのidのインデックス
+                const movedIdIndex = newLists.findIndex(
+                    ({
+                        id
+                    }) => id === beforeMoveId
+                );
+                //動かしたアイテムのidのひとつ上
+                afterMoveId =
+                    movedIdIndex - 1 >= 0 ? newLists[movedIdIndex - 1].id : null;
+            }
+            //同じリスト内の移動のとき
+            if (newLists.length === oldLists.length) {
+                beforeMoveId = oldLists[this.moveOldIndex].id;
+                //動かしたアイテムのidのインデックス
+                const movedIdIndex = newLists.findIndex(
+                    ({
+                        id
+                    }) => id === beforeMoveId
+                );
+                //動かしたアイテムのidのひとつ上
+                afterMoveId =
+                    movedIdIndex - 1 >= 0 ? newLists[movedIdIndex - 1].id : null;
+            }
+            let changeListId = this.list.id;
+            axios
+                .post("/items/move", {
+                    id: beforeMoveId,
+                    to_id: afterMoveId,
+                    list_id: changeListId,
+                })
+                .then((response) => {});
+        },
+        moveId: function (event) {
+            this.moveOldIndex = event.oldIndex;
+        },
+        ItemToListInfo: function (itemInfo) {
+            this.$emit("ListToBoardInfo", itemInfo, this.title, this.list.id);
+        },
     },
-    ItemToListInfo: function (itemInfo) {
-      this.$emit('ListToBoardInfo', itemInfo, this.title, this.list.id)
-    },
-  },
 };
 </script>
